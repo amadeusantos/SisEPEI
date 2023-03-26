038712c0d71bdde0c9688ecb4d50141f11a3c6c4
Amadeu Santos <amadeusantos31@gmail.com>
2023-03-25 22:34:02 -0300

    criando o repositorio de usuário

---
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/usuario/UsuarioRepositorio.java
@@ -0,0 +1,7 @@
+package br.upe.sisepei.sisepei.core.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
+
+}
