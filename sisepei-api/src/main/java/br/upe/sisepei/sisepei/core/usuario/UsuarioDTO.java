2ed78a9ea3701acd4144ac9e2a676c645f775333
Amadeu Santos <amadeusantos31@gmail.com>
2023-03-25 22:38:34 -0300

    criando o UsuarioDTO

---
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/usuario/UsuarioDTO.java
@@ -0,0 +1,20 @@
+package br.upe.sisepei.sisepei.core.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UsuarioDTO {
+
+	@NotBlank
+	private String nome;
+
+	@NotBlank
+	private String email;
+	
+	@NotBlank
+	@Size(min = 6)
+	private String senha;
+
+}
