8b7d241d1b405fde4b3d07336cb1b83d975d5205
Amadeu Santos <amadeusantos31@gmail.com>
2023-03-26 00:38:51 -0300

    fazendo exception para itens não encontrados

---
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/base/exception/NaoEncontradoException.java
@@ -0,0 +1,10 @@
+package br.upe.sisepei.sisepei.base.exception;
+
+public class NaoEncontradoException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public NaoEncontradoException(String mensagem) {
+		super(mensagem);
+	}
+
+}
