5554c285a4d640052ddb7ca32fc8c6e49628ff76
toni <antoniojsereno@gmail.com>
2023-03-27 15:25:11 -0300

    Cadastro Usuario Geral
    Desenvolvimento da funcionalidade "finalizado", ele esta funcional, mas tenho que testar em algum programa para ver como esta o arquivo json que cadastro esta soltando. No mas, tudo certo.

--- sisepei-web/src/App.js
+++ sisepei-web/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { CadastroUsuarioGeral } from './pages/Cadastro UsuarioGeral/CadastroUsuarioGeral';
+import { CadastroConcluido } from './pages/Cadastro UsuarioGeral/CadastroConcluido';
+
+
 
 function App() {
   return (
+    <>
     <div>
-      Hello World
+      <Routes>
+        <Route path="/usuario/geral/cadastro" element={<CadastroUsuarioGeral />} />
+        <Route path="/usuario/geral/cadastro/concluido" element={<CadastroConcluido />} />
+      </Routes>
     </div>
+    </>
   );
+  
 }
 
 export default App;
---
+++ sisepei-web/src/pages/Cadastro UsuarioGeral/CadastroUsuarioGeral.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../lib/Api";
+import './CadastroUsuarioGeral.css';
+
+
+export function CadastroUsuarioGeral(){
+    //declaraçoes
+    const navigate =  useNavigate();
+
+    //constantes com useState que serao utilizadas
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [confirmaSenha, setConfirmaSenha] = useState("");
+
+    async function cadastrarUsuarioGeral(event) {
+        event.preventDefault();
+
+        await api
+        .post("/usuario/geral/cadastro", {
+            nome: nome,
+            email: email,
+            senha: senha,
+        })
+        .then(() => navigate("/usuario/geral/cadastro/concluido"))
+        .catch((err) => console.log(err));
+
+        setNome("")
+        setEmail("")
+        setSenha("")
+        setConfirmaSenha("")
+    }
+    
+    return(
+        <>
+            <div id="divGeral">
+                <h3>Cadastro de Usuario Geral</h3>
+                <p>Preencha o Cadastro com as informaçoes pertinentes</p>
+
+                <label htmlFor="nome">Nome:</label>
+                <input id="nome" type="text" required
+                onClick={(event)=> setNome(event.target.value)} />
+                <br/>
+                <label htmlFor="email">Email:</label>
+                <input id="email" type="email" required
+                onClick={(event)=> setEmail(event.target.value)} />
+                <br/>
+                <label htmlFor="senha">Senha:</label>
+                <input id="senha" type="password" required
+                 onClick={(event)=> setSenha(event.target.value)} />
+                <br/>
+                <label htmlFor="confirmaSenha">Comfirme sua senha:</label>
+                <input id="confirmaSenha" type="password" required
+                 onClick={(event)=> setConfirmaSenha(event.target.value)} />
+                <br/>
+                <button
+                 onClick={(event) => cadastrarUsuarioGeral(event)}
+                >Cadastrar</button> <button>Voltar</button>
+            </div>
+        </>
+    );
+}
