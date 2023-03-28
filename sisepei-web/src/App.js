3cdaca7fde6b2897784f53e376fbf8b123c61b5c
toni <antoniojsereno@gmail.com>
2023-03-28 12:37:38 -0300

    Cadastro Edital e Coordenador
    Somente os Componente do React e suas Rotas, sem estilização com CSS

--- sisepei-web/src/App.js
+++ sisepei-web/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { CadastroUsuarioGeral } from './pages/Cadastro UsuarioGeral/CadastroUsuarioGeral';
 import { CadastroConcluido } from './pages/Cadastro UsuarioGeral/CadastroConcluido';
+import { CadastroCoordenador } from './pages/Cadastro UsuarioGeral/CadastroCoordenador';
+import { CadastroEditais } from './pages/Cadastro UsuarioGeral/CadastroEditais';
 
 
 
@@ -11,7 +13,9 @@ function App() {
     <div>
       <Routes>
         <Route path="/usuario/geral/cadastro" element={<CadastroUsuarioGeral />} />
-        <Route path="/usuario/geral/cadastro/concluido" element={<CadastroConcluido />} />
+        <Route path="/cadastro/concluido" element={<CadastroConcluido />} />
+        <Route path="usuario/coordenador/cadastro" element={<CadastroCoordenador />} />
+        <Route path="/edital/cadastro" element={<CadastroEditais />} />
       </Routes>
     </div>
     </>
--- sisepei-web/src/pages/Cadastro UsuarioGeral/CadastroCoordenador.js
+++ sisepei-web/src/pages/Cadastro UsuarioGeral/CadastroCoordenador.js
@@ -1,42 +1,53 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api } from "../../lib/Api";
-import './CadastroUsuarioGeral.css';
 
 
-export function CadastroUsuarioGeral(){
+export function CadastroCoordenador(){
     //declaraçoes
     const navigate =  useNavigate();
 
     //constantes com useState que serao utilizadas
+    const [tipo, setTipo] = useState("");
     const [nome, setNome] = useState("");
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
     const [confirmaSenha, setConfirmaSenha] = useState("");
 
-    async function cadastrarUsuarioGeral(event) {
+    async function cadastrarCoordenador(event) {
         event.preventDefault();
 
         await api
-        .post("/usuario/geral/cadastro", {
+        .post("usuario/coordenador/cadastro", {
+            tipo: tipo,
             nome: nome,
             email: email,
             senha: senha,
         })
-        .then(() => navigate("/cadastro/concluido"))
+        .then(() => navigate("cadastro/concluido"))
         .catch((err) => console.log(err));
 
-        setNome("")
-        setEmail("")
-        setSenha("")
-        setConfirmaSenha("")
+        setTipo("");
+        setNome("");
+        setEmail("");
+        setSenha("");
+        setConfirmaSenha("");
     }
     
     return(
         <>
             <div id="divGeral">
-                <h3>Cadastro de Usuario Geral</h3>
-                <p>Preencha o Cadastro com as informaçoes pertinentes</p>
+                <h1>Quem faz cadastro de Coordenador é o ADMIN !! Mas o adm que define a sneha do Coordenador ou terá um sistema de "primeira acesso" onde o coordenador sera aconselhado a definir uma nova senha?</h1>
+                <h3>Cadastro de Coordenador no Sistema</h3>
+                <p>Preencha o Cadastro com as informaçoes pertinentes sobre o coordenador</p>
+
+                <label htmlFor="selectCoordenador">Selecione o Tipo de Coordenador:</label>
+                <select id="selectCoordenador" required>
+                    <option value="" disabled>--- Select ---</option>
+                    <option value="extensao">Coordenador de Extenção</option>
+                    <option value="pesquisa">Coordenador de Pesquisa</option>
+                    <option value="inovacao">Coordenador de Inovação</option>
+                </select>
 
                 <label htmlFor="nome">Nome:</label>
                 <input id="nome" type="text" required
@@ -47,15 +58,15 @@ export function CadastroUsuarioGeral(){
                 onClick={(event)=> setEmail(event.target.value)} />
                 <br/>
                 <label htmlFor="senha">Senha:</label>
-                <input id="senha" type="password" required
+                <input id="senha" type="password" 
                  onClick={(event)=> setSenha(event.target.value)} />
                 <br/>
                 <label htmlFor="confirmaSenha">Comfirme sua senha:</label>
-                <input id="confirmaSenha" type="password" required
+                <input id="confirmaSenha" type="password" 
                  onClick={(event)=> setConfirmaSenha(event.target.value)} />
                 <br/>
                 <button
-                 onClick={(event) => cadastrarUsuarioGeral(event)}
+                 onClick={(event) => cadastrarCoordenador(event)}
                 >Cadastrar</button> <button>Voltar</button>
             </div>
         </>
