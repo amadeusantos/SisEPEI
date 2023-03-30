94a64cd10b788948da0124959b88e589e7b67069
Ana Laura <analaurass45@gmail.com>
2023-03-30 18:02:43 -0300

    criando o DTO

---
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/edital/modelo/EditalDTO.java
@@ -0,0 +1,31 @@
+package br.upe.sisepei.sisepei.core.edital.modelo;
+
+import java.io.File;
+import java.util.Date;
+
+import br.upe.sisepei.sisepei.core.usuario.modelo.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class EditalDTO {
+	
+	@NotBlank
+	private String titulo;
+
+	@NotBlank
+	private String descricao;
+	
+	@NotBlank
+	private String requisitos;
+	
+	@NotBlank
+	private File edital;
+	
+	@NotBlank
+	private Date prazo;
+	
+	@NotBlank
+	private Usuario coordenador;
+
+}
