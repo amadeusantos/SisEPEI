c693fe942210bc5e879a9ea29191fe225407d858
Ana Laura <analaurass45@gmail.com>
2023-03-31 18:47:00 -0300

    criando rota update no controller

--- sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/edital/controller/EditalController.java
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/edital/controller/EditalController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,18 @@ public class EditalController {
 			Edital result = editalServico.criarEdital(edital);			
 			return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> updateEdital(@PathVariable Long id, @RequestBody EditalDTO edital){
+		try{
+			editalServico.updateEdital(id, edital);
+		} catch(NaoEncontradoException e){
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deletarEdital(@PathVariable Long id){
