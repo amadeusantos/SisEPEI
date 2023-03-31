6fd2ebd44825fe20eecaf0a86f3f0b20e1fb187f
Ana Laura <analaurass45@gmail.com>
2023-03-31 18:45:50 -0300

    criando update do edital

--- sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/edital/EditalServico.java
+++ sisepei-api/src/main/java/br/upe/sisepei/sisepei/core/edital/EditalServico.java
@@ -1,5 +1,7 @@
 package br.upe.sisepei.sisepei.core.edital;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,25 +32,55 @@ public class EditalServico {
 	}
 	
 	public Edital criarEdital(EditalDTO edital) {
+		Date dataHoraAtual = new Date(); 
+		
 		Edital editalSalvar = new Edital();
 		editalSalvar.setTitulo(edital.getTitulo());
 		editalSalvar.setDescricao(edital.getDescricao());
 		editalSalvar.setEdital(edital.getEdital());
 		editalSalvar.setRequisitos(edital.getRequisitos());
+		editalSalvar.setTipo(edital.getTipo());
 		editalSalvar.setCoordenador(edital.getCoordenador());
-		editalSalvar.setPrazo(edital.getPrazo());
+		editalSalvar.setPrazo(dataHoraAtual);
 		
 		return repositorio.save(editalSalvar);
 	}
 	
-	/*public Edital updateEdital(Long id, EditalDTO edital) throws NaoEncontradoException{
+	public void updateEdital(Long id, EditalDTO edital) throws NaoEncontradoException{
 		Optional<Edital> editalExists = repositorio.findById(id);
 		if(editalExists.isEmpty()) {
 			throw new NaoEncontradoException("Não existe nenhum edital correspondente a esse id");
 		}
+
+		Date atual = new Date();
+		Edital editalExistente = repositorio.findById(id).get();
+
+		if(edital.getTitulo() != null){
+			editalExistente.setTitulo(edital.getTitulo());
+		}
+
+		if(edital.getDescricao() != null){
+			editalExistente.setDescricao(edital.getDescricao());
+		}
+
+		if(edital.getEdital() != null){
+			editalExistente.setEdital(edital.getEdital());
+		}
+
+		if(edital.getRequisitos() != null){
+			edital.setRequisitos(edital.getRequisitos());
+		}
+
+		if(edital.getTipo() != null){
+			editalExistente.setTipo(edital.getTipo());
+		}
+
+		editalExistente.setPrazo(atual);			
+		repositorio.save(editalExistente);
+
+
 		
-		
-	}*/
+	}
 		
 	public void removerEdital(Long id) throws NaoEncontradoException{
 		Optional<Edital> edital = repositorio.findById(id);
