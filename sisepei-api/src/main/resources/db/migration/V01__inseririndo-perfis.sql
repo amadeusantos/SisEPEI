8dc781291d12994b0900e9c1b800cd0c9586bdc2
Amadeu Santos <amadeusantos31@gmail.com>
2023-03-31 23:00:43 -0300

    tentando configurar flyway inserir perfis

---
+++ sisepei-api/src/main/resources/db/migration/V01__inseririndo-perfis.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO perfil values (1, "COORDENADOR_EXTENSAO");
+INSERT INTO perfil values (2, "COORDENADOR_PESQUISA");
+INSERT INTO perfil values (3, "COORDENADOR_INOVACAO");
+INSERT INTO perfil values (4, "ADMINSTRADOR");
