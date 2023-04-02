8eac0d664422372bd0bf155e2bdf1ed3ffb5b517
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-04-02 13:35:24 -0300

    Infocards
    Agora os infocards conseguem pegar dados do backend pra serem renderizados

--- sisepei-web/src/components/pages/pagina-inicial-usuario/Index.jsx
+++ sisepei-web/src/components/pages/pagina-inicial-usuario/Index.jsx
@@ -4,31 +4,48 @@ import './style.css';
 import BotaoCadastrar from '../../layout/BotaoCadastrar';
 import SearchBar from '../../layout/SearchBar';
 import Card from '../../layout/InfoEditais';
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+
 
 export function PaginaInicial(props) {
+  const [cardsData, setCardsData] = useState([]);
+  useEffect(() => {
+    console.log("Fetching cards...");
+    Axios
+    //inserir o link no get
+      .get(``)
+      .then((response) => {
+        setCardsData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
     <body id='page1'>
+
       <TopBar />
       <h1 className='welcome'>Bem vindo!</h1>
       <hr className='myhr' />
       <h1 className='editaiswelcome'>Editais</h1>
+
       <div className='button-search'>
-        <BotaoCadastrar />
-        <SearchBar />
+      <BotaoCadastrar />
+      <SearchBar />
       </div>
+      {/* Array.isArray vai checar se a data sendo recebida pelo back é um array, caso contrário não vai renderizar nada
+      coloquei aqui pra não ficar dando erro, mas caso preciso é só remover. */}
+      {Array.isArray(cardsData) && cardsData.map((card) => (
       <Card
-      name="Edital"
-      description="Edtal de extensão bla bla bla bla bla blabal ldkjjsdjkashdhgshdgj
-sdhsdjhgggfhdgfghdfhdcvdbc ndbchdbhdfvdgvgdvdcbhjcbdbcj"
-      type = "Extensão"
-    >
-    </Card>
-    <Card
-      name="Edital1"
-      description="Esse edital nao serve pra nada"
-      type = "Extensão"
-    >
-    </Card>
+        key={card.id}
+        name={card.name}
+        type={card.type}
+        description={card.description}
+      />
+    ))}
     </body>
   );
 }
