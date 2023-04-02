3278dd372cc8e8366931307bb6bf21565623cf37
Camila Claudino <camila.claudino@upe.br>
2023-04-02 18:00:57 -0300

    criação do header

--- sisepei-web/src/App.js
+++ sisepei-web/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Login from "./pages/Login/Login";
+import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
+      <Header />
+      
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
