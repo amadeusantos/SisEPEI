faa0bf0a11b2d418a43f5c023add2e542def8873
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-04-03 23:56:58 -0300

    Barra de pesquisa
    Barra de pesquisa implementada. Ela consegue filtrar por nome e tipo. OBS: Quando for fazer a integração com o back tirar os cardsData criados no useState e tirar o setCardsData do comentário.

--- sisepei-web/src/components/layout/SearchBar.jsx
+++ sisepei-web/src/components/layout/SearchBar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './style.css';
 
-function SearchBar() {
+function SearchBar ({searchTerm, setSearchTerm}){
   return (
     <div className='search-bar'>
       <input 
         type="text" 
         placeholder='Buscar...' 
+        value={searchTerm}
+        onChange={event => 
+          {setSearchTerm(event.target.value)
+          }}
       />
     </div>
   );
--- sisepei-web/src/components/pages/pagina-inicial-usuario/Index.jsx
+++ sisepei-web/src/components/pages/pagina-inicial-usuario/Index.jsx
@@ -10,22 +10,27 @@ import Filter from '../../layout/Filter';
 
 
 export function PaginaInicial() {
-  const [cardsData, setCardsData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+  const [cardsData, setCardsData] = useState([{ id: 1, name: "Edital 1", type: "Tipo 1", description: "Descrição 1" },
+  { id: 2, name: "Edital 2", type: "Tipo 2", description: "Descrição 2" },
+  { id: 3, name: "Edital 3", type: "Tipo 1", description: "Descrição 3" },
+  { id: 4, name: "Edital 4", type: "Tipo 2", description: "Descrição 4" },]);
   useEffect(() => {
     console.log("Fetching cards...");
     Axios
     //inserir o link no get
       .get(``)
       .then((response) => {
-        setCardsData(response.data);
+        //comentei esse setCardsData porque ele impede que eu teste com os cards que tem ali em cima.
+        // setCardsData(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
-
-
-  return (
+  const filteredCards = cardsData.filter(card => card.name.toLowerCase().includes(searchTerm.toLowerCase())
+      || card.type.toLowerCase().includes(searchTerm.toLowerCase()))
+  return ( 
     <div id='page1'>
 
       <TopBar />
@@ -36,26 +41,23 @@ export function PaginaInicial() {
       <div className='button-search'>
         <BotaoCadastrar />
           <div className='search-filter'>
-            <SearchBar />
+          <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             <Filter/>
           </div>
       </div>
+
       {/* Array.isArray vai checar se a data sendo recebida pelo back é um array, caso contrário não vai renderizar nada,
       coloquei aqui pra não ficar dando erro, mas caso preciso é só remover. */}
-      {Array.isArray(cardsData) && cardsData.map((card) => (
+
+      {Array.isArray(filteredCards) && filteredCards.map((card) => (
       <Card
         key={card.id}
         name={card.name}
         type={card.type}
         description={card.description}
       />
-
     ))}
 
-    {/* teste pra ver se os cards tão funcionando */}
-     <Card name="edital" type="Extensão" description="fjkdsalfjlksdafjksdalfjlksdjfls" />
-    <Card name="edital" type="Extensão" description="fjkdsalfjlksdafjksdalfjlksdjfls" />
-    <Card name="edital" type="Extensão" description="fjkdsalfjlksdafjksdalfjlksdjfls" />
     </div>
   );
 }
