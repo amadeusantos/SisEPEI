f5be5dddd54f931428cf872fc39f957ac0ecb69b
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-04-12 00:09:38 -0300

    Organizando a pagina

--- sisepei-web/src/components/pages/pagina-inicial-usuario/Index.jsx
+++ sisepei-web/src/components/pages/pagina-inicial-usuario/Index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Style.css';
-import BotaoCadastrar from '../../layout/BotaoCadastrar';
 import SearchBar from '../../layout/SearchBar';
 import Card from '../../layout/InfoEditais';
 import { useEffect, useState } from 'react';
@@ -11,7 +10,7 @@ import Filter from '../../layout/Filter';
 export function PaginaInicial() {
   const [searchTerm, setSearchTerm] = useState('')
   const [cardsData, setCardsData] = useState([{ id: 1, name: "Edital 1", type: "Tipo 1", description: "Descrição 1" },
-  { id: 2, name: "Edital 2", type: "Tipo 2", description: "Descrição 2" },
+  { id: 2, name: "Edital 2", type: "Tipo 2", description: "Lorem ipsum sit amet, consectetur adipiscing elit. Nulla facilisi. Vestibulum consectetur, sapien auctor consectetur fermentum, sem mauris bibendum mauris, id congue diam tellus ut nisl. Sed ac luctus leo. Donec euismod justo eu libero pretium ultricies. Sed aliquam, nibh vel ullamcorper feugiat, mi purus egestas nunc, vel sodales mi massa in metus. Aenean auctor, ipsum a tincidunt malesuada, felis dui convallis tortor, in fringilla elit elit nec ante. Sed tristique nibh nibh, non dapibus massa pulvinar eu. Sed efficitur metus nec odio blandit interdum. Sed blandit lacus ac elit imperdiet, non gravida metus pharetra. Sed non lacus tincidunt, bibendum metus eget, vestibulum ex. In hac habitasse platea dictumst. Maecenas rhoncus orci a libero efficitur, sit amet semper felis dignissim. Suspendisse potenti." },
   { id: 3, name: "Edital 3", type: "Tipo 1", description: "Descrição 3" },
   { id: 4, name: "Edital 4", type: "Tipo 2", description: "Descrição 4" },]);
   useEffect(() => {
