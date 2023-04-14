7a850433abb4bcdbbab02a2239f69f5904fd117b
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-04-14 20:31:37 -0300

    Fix

--- sisepei-web/src/App.js
+++ sisepei-web/src/App.js
@@ -4,7 +4,7 @@ import { CadastroUsuarioGeral } from './pages/Cadastro/CadastroUsuarioGeral';
 import { CadastroConcluido } from './pages/Cadastro/CadastroConcluido';
 import { MudancaPermicao } from './pages/Cadastro/MudancaPermicao';
 import { CadastroEditais } from './pages/Cadastro/CadastroEditais';
-import Header from './Components/Header/Index';
+import Header from './components/Header/Index';
 import Login from "./pages/Login/Login";
 import PaginaInicial from './pages/pagina-inicial-usuario/Index';
 import React from 'react';
