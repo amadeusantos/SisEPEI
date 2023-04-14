a869d5ff4d1676673a4a06ce917f0ad99d39dbff
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-04-14 20:12:09 -0300

    Update modal
    Quase pronto, falta fazer o botão de baixar funcionar, provavelmente vou ter que adicionar o id ao card e ao modal.

--- sisepei-web/src/App.js
+++ sisepei-web/src/App.js
@@ -4,9 +4,9 @@ import { CadastroUsuarioGeral } from './pages/Cadastro/CadastroUsuarioGeral';
 import { CadastroConcluido } from './pages/Cadastro/CadastroConcluido';
 import { MudancaPermicao } from './pages/Cadastro/MudancaPermicao';
 import { CadastroEditais } from './pages/Cadastro/CadastroEditais';
-import Header from './Components/Header/Index';
+import Header from './components/Header/Index';
 import Login from "./pages/Login/Login";
-import PaginaInicial from './Components/pages/pagina-inicial-usuario/Index';
+import PaginaInicial from './pages/pagina-inicial-usuario/Index';
 import React from 'react';
 
 function App() {
--- sisepei-web/src/components/layout/Modal.jsx
+++ sisepei-web/src/components/layout/Modal.jsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import './style.css';
 
-function Modal({ closeModal, name, type, description }) {
-    return (
-      <div className='modalBackground'>
-        <div className='modalBox'>
-          <div className='modalContent'>
-            <div className='modalProp'>
-              <span>Nome:</span>
-              <p>{name}</p>
-            </div>
-            <div className='modalProp'>
-              <span>Tipo:</span>
-              <p>{type}</p>
-            </div>
-            <div className='modalProp'>
-              <span>Descrição:</span>
-              <p>{description.substring(0, 850)}</p>
-            </div>
-          </div>
-          <button onClick={() => closeModal(false)}>Fechar</button>
-        </div>
-      </div>
-    )
+function Modal({ closeModal, name, type, description, term, requirements, coordinator }) {
+  const baixarArquivo = () => {
+    console.log("Baixando Arquivo")
   }
-  
+  return (
+    <div className='modalBackground'>
+      <div className='modalBox'>
+        <div className='modalContent'>
+          <div className='modalProp'>
+            <span>Título:</span>
+            <p>{name}</p>
+          </div>
+          <div className='modalProp'>
+            <span>Coordenador:</span>
+            <p>{coordinator}</p>
+          </div>
+          <div className='modalProp'>
+            <span>Tipo:</span>
+            <p>{type}</p>
+          </div>
+          <div className='modalProp'>
+            <span>Prazo:</span>
+            <p>{term}</p>
+          </div>
+          <div className='modalProp'>
+            <span>Requisitos:</span>
+            <p>{requirements}</p>
+          </div>
+          <div className='modalProp'>
+            <span>Descrição:</span>
+            <p>{description.substring(0, 600)}</p>
+          </div>
+          <div className='modalProp'>
+            <span>Baixar Edital:</span>
+            <button className="baixar"onClick={baixarArquivo}>Baixar Arquivo</button>
+          </div>
+        </div>
+        <button className="fechar"onClick={() => closeModal(false)}>Fechar</button>
+      </div>
+    </div>
+  )
+}
+
 
 export default Modal;
