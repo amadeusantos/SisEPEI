beee749bf06eec053f81f6e68518c7469440a2a2
amadeusantos <amadeusantos31@gmail.com>
2024-10-06 16:52:07 -0300

    docs: add JSDoc in Button and InputText

--- sisepei-web/src/components/atoms/Button/index.jsx
+++ sisepei-web/src/components/atoms/Button/index.jsx
@@ -1,6 +1,17 @@
 import ButtonBootstrap from "react-bootstrap/Button";
 import "./style.css";
 
+/**
+ * A button component with customizable color, type, and click handler.
+ * 
+ * @param {Object} props - The properties object.
+ * @param {'primary'|'secondary'|'danger'} [props.color='primary'] - The color of the button. Default is 'primary'.
+ * @param {'button'|'submit'|'reset'} [props.type='button'] - The type of the button. Default is 'button'.
+ * @param {Function} [props.onClick=() => {}] - The function to be called when the button is clicked. Default is an empty function.
+ * @param {React.ReactNode} props.children - The content or elements to be displayed inside the button.
+ * 
+ * @returns {React.FC} The rendered button element.
+ */
 export function Button({
   color = "primary",
   type = "button",
--- sisepei-web/src/components/molecules/InputText/index.jsx
+++ sisepei-web/src/components/molecules/InputText/index.jsx
@@ -1,5 +1,19 @@
 import Form from "react-bootstrap/Form";
 
+/**
+ * A form input component with a label and customizable properties.
+ * 
+ * @param {Object} props - The properties object.
+ * @param {string} props.label - The label text associated with the input field.
+ * @param {boolean} [props.required=false] - Whether the input is required. Default is `false`.
+ * @param {string} [props.placeholder=''] - The placeholder text for the input field. Default is an empty string.
+ * @param {string} props.name - The name attribute for the input field.
+ * @param {string} props.value - The current value of the input field.
+ * @param {Function} props.onChange - The function to handle changes in the input value.
+ * @param {string} [props.type='text'] - The type of input field (e.g., text, password, email). Default is 'text'.
+ * 
+ * @returns {React.FC} The rendered input field component.
+ */
 export function InputText({
   label,
   required,
@@ -8,7 +22,6 @@ export function InputText({
   value,
   onChange,
   type = "text",
-  isForm = false,
 }) {
   const setValue = (e) => onChange(e.target.value);
 
