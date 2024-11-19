721e61dbb1d85ea06d5ba8b08d11788d952646aa
mightbehappyy <pedroluiz0201690@gmail.com>
2024-11-18 23:48:20 -0300

    refac: remove type: module form package json and change vite.config.mjs extension

--- sisepei-web/vite.config.js
+++ sisepei-web/vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+      host:'0.0.0.0',
+      port: 5173
+    },
   plugins: [react()],
   test: {
     environment: "jsdom",
