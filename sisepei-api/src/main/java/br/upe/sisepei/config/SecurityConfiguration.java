c3e12c425521e23f89a4fb9193aa39a85c0de718
Rafael Souza <rafael.jose@ferreri.co>
2024-11-30 17:16:17 -0300

    dispatch 401 instead of 403

--- sisepei-api/src/main/java/br/upe/sisepei/config/SecurityConfiguration.java
+++ sisepei-api/src/main/java/br/upe/sisepei/config/SecurityConfiguration.java
@@ -3,11 +3,13 @@ package br.upe.sisepei.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
@@ -29,6 +31,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .exceptionHandling(exp -> exp.authenticationEntryPoint(new
+                        HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
