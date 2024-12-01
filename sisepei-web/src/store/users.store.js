29544d3456e3977fa37d5cf886668c5b37b79e0b
Rafael Souza <rafael.jose@ferreri.co>
2024-12-01 17:25:09 -0300

    chore: create useUsersPermissionsRows store

--- sisepei-web/src/store/users.store.js
+++ sisepei-web/src/store/users.store.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
@@ -9,6 +10,54 @@ export function useUsers() {
         queryKey: ['users'],
         queryFn: getUsers
     })
+}
+
+function createData(email, isExtensionCoordinator, isResearchCoordinator, isInovationCoordinator, isAdmin) {
+    return { email, isExtensionCoordinator, isResearchCoordinator, isInovationCoordinator, isAdmin };
+}
+
+function mountUserRoles(user) {
+    const userProfiles = user.profiles
+    const roles = {
+        isExtensionCoordinator: false,
+        isResearchCoordinator: false,
+        isInovationCoordinator: false,
+        isAdmin: false
+    }
+
+    for (const profile of userProfiles) {
+        if (profile.name == 'COORDENADOR_EXTENSAO') {
+            roles.isExtensionCoordinator = true
+        }
+        if (profile.name == 'ADMINISTRADOR') {
+            roles.isAdmin = true
+        }
+        if (profile.name == 'COORDENADOR_PESQUISA') {
+            roles.isResearchCoordinator = true
+        }
+        if (profile.name == 'COORDENADOR_INOVACAO') {
+            roles.isInovationCoordinator = true
+        }
+    }
+
+    return roles
+}
+
+export function useUsersPermissionsRows() {
+    const queryUsers = useUsers();
+    const rows = useMemo(() => queryUsers.data?.map(user => {
+        const roles = mountUserRoles(user)
+
+        return createData(
+            user.email,
+            roles.isExtensionCoordinator,
+            roles.isResearchCoordinator,
+            roles.isInovationCoordinator,
+            roles.isAdmin,
+        )
+    }), [queryUsers.data]);
+
+    return { ...queryUsers, data: rows }
 }
 
 export function useLogout() {
---
+++ sisepei-web/src/utils/enums.js
@@ -0,0 +1,6 @@
+export const ROLES_MAP = {
+    ADMINISTRADOR: 'Administrador Geral',
+    COORDENADOR_EXTENSAO: 'Coordenador de Extensão',
+    COORDENADOR_INOVACAO: 'Coordenador de Inovação',
+    COORDENADOR_PESQUISA: 'Coordenador de Pesquisa'
+}
